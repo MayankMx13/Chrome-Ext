65ebdd52fe156c3e3c4ab56a79251a20503c85c0
MayankMx13 <mayankmishra3214@gmail.com>
2024-09-19 12:32:57 +0530

    task done

---
+++ popup.js
@@ -0,0 +1,9 @@
+document
+  .getElementById("getTitleButton")
+  .addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const currentTab = tabs[0];
+      document.getElementById("tabTitle").textContent =
+        "Current Tab Title: " + currentTab.title;
+    });
+  });
